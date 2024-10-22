9cc5df948f68ca61ae781f5f93795663df76f2bd
Ryan <60459219+rshortbull@users.noreply.github.com>
2024-10-22 10:50:48 -0700

    Update Card.py

--- Card.py
+++ Card.py
@@ -9,6 +9,39 @@ wn.title("E Card")
 wn.bgpic("BG2.png")
 
 
+wn.register_shape("coww.gif")
+wn.addshape("ufo.gif")
+
+
+cow = turtle.Turtle()
+cow.speed(0)
+cow.setheading(90)
+cow.shape("coww.gif")
+cow.color("red")
+cow.penup()
+cow.goto(200,-50)
+
+ufo = turtle.Turtle()
+ufo.hideturtle
+ufo.speed(0)
+ufo.setheading(90)
+ufo.shape("ufo.gif")
+ufo.penup()
+ufo.goto(500,500)
+ufo.speed(1)
+
+
+def move_ufo(x, y):
+    ufo.showturtle()  
+    ufo.goto(200, 150)  
+    time.sleep(1) 
+    cow.hideturtle() 
+    ufo.goto(500, 500)  
+    ufo.hideturtle() 
+
+# Bind the cow turtle to respond to clicks
+cow.onclick(move_ufo)
+
 def change_color(x, y):
     wn_width = wn.window_width()
     
@@ -19,8 +52,10 @@ def change_color(x, y):
     # Only fade to black if clicked on the left or right side
     if x > right_boundary / 1.3:  # Right half of the screen
         wn.bgpic("BG1.png")
+        cow.hideturtle()
     elif x < left_boundary / 1.3:  # Left half of the screen
         wn.bgpic("BG2.png")
+        cow.showturtle()
 
 # Bind the click event (clicking anywhere on the wn)
 wn.onclick(change_color)
